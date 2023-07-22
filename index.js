583420a3b2cab85ffde627d628c8c01fd728403a
T.Sanyi <s.truczkai@gmail.com>
2023-07-22 17:40:31 +0200

    rework index.js to remove count variable

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el") //working with the DOM (Document Object Model)
-let count = 0
 
 function increment() {
-    count++
-    countEl.textContent = count
+    countEl.textContent++
 }
 
 function save() {
-    saveEl.textContent += count + " - "
-    }
+    saveEl.textContent += countEl.textContent + " - "
+    countEl.textContent = 0
+}
